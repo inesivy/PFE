73975cd6259bc76038bf5b210bcb615f1d9742e4
ofouzi <33906235+ofouzi@users.noreply.github.com>
2018-03-17 11:38:09 +0100

    Update dyna.sh

--- dyna.sh
+++ dyna.sh
@@ -2,12 +2,18 @@
 
 echo "lauching router 1"
 
-.//dynamips/stable/stable/dynamips -P 7200 --idle-pc 0x6077b2bc -i 1 -X -T 2001 \
- -p 1:PA-FE-TX -s 1:0:udp:10003:127.0.0.1:10005 -p 2:PA-FE-TX -s 2:0:udp:10007:127.0.0.1:10006 /net/cremi/ofouzi/espaces/travail/PFE/c7200-jk9s-mz.124-13b.image &
+./dynamips/build/stable/dynamips -P 7200 --idle-pc 0x6077b2bc -i 1 -X -T 2001 \
+ -p 2:PA-FE-TX -s 2:0:udp:10003:127.0.0.1:10002 \
+ -p 3:PA-FE-TX -s 3:0:udp:10100:127.0.0.1:10101 \
+ /net/cremi/ofouzi/espaces/travail/PFE/c7200-jk9s-mz.124-13b.image &
 
 echo "lauching router 2"
 
-.//dynamips/stable/stable/dynamips -P 7200 --idle-pc 0x6077b2bc -i 2 -X -T 2002 \
- -p 1:PA-FE-TX -s 1:0:udp:10005:127.0.0.1:10003 -p 2:PA-FE-TX -s 2:0:udp:10009:127.0.0.1:10008 /net/cremi/ofouzi/espaces/travail/PFE/c7200-jk9s-mz.124-13b.image &
+./dynamips/build/stable/dynamips -P 7200 --idle-pc 0x6077b2bc -i 2 -X -T 2002 \
+ -p 1:PA-FE-TX -s 1:0:udp:10005:127.0.0.1:10004 \
+ -p 2:PA-FE-TX -s 2:0:udp:10007:127.0.0.1:10006 \
+ -p 3:PA-FE-TX -s 3:0:udp:10101:127.0.0.1:10100 \
+ /net/cremi/ofouzi/espaces/travail/PFE/c7200-jk9s-mz.124-13b.image &
+
 
 
