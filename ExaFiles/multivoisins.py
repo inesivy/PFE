6d7b14aacd62970031446f12e056c7cc087351e7
ofouzi <33906235+ofouzi@users.noreply.github.com>
2018-03-26 12:48:17 +0200

    Update multivoisins.py

--- ExaFiles/multivoisins.py
+++ ExaFiles/multivoisins.py
@@ -2,7 +2,7 @@
 
 import sys
 import time
-
+#fonctionne qu'avec la topologie à deux routeurs 
 messages = [
 	'neighbor 192.168.44.2 announce route 192.168.0.0/24 next-hop 192.168.33.1',
 	'neighbor 192.168.44.1 router-id 192.168.44.1, neighbor 192.168.44.2 announce route 192.168.55.2/25 next-hop 192.168.55.1',
